77e5aa3b28cffcabc80f1af9fafb884669efdf64
Your Name <1585499252@qq.com>
2023-10-07 15:24:14 +0800

    发布

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:/<REPO>.git master:gh-pages
+git push -f git@github.com:zhangshengpeng2021/vuedemo.git master:gh-pages
 
 cd -
