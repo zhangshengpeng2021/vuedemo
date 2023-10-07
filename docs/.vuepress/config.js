6d31ac68a87407b025f9b85b7a321aae4b53a4bb
Your Name <1585499252@qq.com>
2023-10-07 15:05:37 +0800

    second

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,23 @@
+const moment = require('moment');
+moment.locale("zh-cn");
 module.exports = {
+    base: "/docs/",
     title: "飞跃高山与大洋的鱼",
     description: "飞跃高山与大洋的鱼的笔记，第二次",
     head: [
         ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
         ['meta', { name: 'author', content: '飞跃高山与大洋的鱼' }],
         ['meta', { name: 'keywords', content: 'vuepress 介绍,vuepress 说明,飞跃高山与大洋的鱼' }],
+    ],
+    plugins: [
+        [
+            '@vuepress/last-updated',
+            {
+                transformer: (timestamp) => {
+                    return moment(timestamp).format("LLLL")
+                }
+            }
+        ]
     ],
     themeConfig: {
         lastUpdated: '更新时间',
